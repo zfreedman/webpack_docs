0a19f52733aa79db42d02c66d16d56c205d1877f
Zach Freedman <zfreedman92@gmail.com>
2018-08-27 14:08:03 -0400

    chore: Cleaned up repo for next section on output management

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import _ from "lodash";
 
-import Icon from "./images/cat.jpeg";
-import data from "./data/data.xml";
-import "./styles/style.css";
-
 function component () {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["hello", "webpack"], " | ");
-  element.classList.add("hello");
-
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-
-  console.log(data);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -7,40 +7,4 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
   },
-
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          "css-loader"
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif|jpeg)$/,
-        use: [
-          "file-loader"
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          "file-loader"
-        ]
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: [
-          "csv-loader"
-        ]
-      },
-      {
-        test: /\.xml$/,
-        use: [
-          "xml-loader"
-        ]
-      }
-    ]
-  }
 };
